baca13f6a8ade3299f0e3c565ada2afd834698cf
Saurabh-827 <saurabhrabta@gmail.com>
2025-03-08 20:31:31 +0530

    added production configuration in config.js

--- models/index.js
+++ models/index.js
@@ -10,6 +10,7 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
+console.log(config.database);
 sequelize = new Sequelize(
 	config.database,
 	config.username,
